95fa83cd55c95ed84dbe7b5182148722b2346bd1
lgcaceres <lg.caceres19@gmail.com>
2020-07-02 20:02:49 -0500

    th1rd

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ import photo from './photo.png'
 const Home = ()=> {
     return (
         <div className="home">
-            <h1 className ="home-title">hi</h1>
+            <div className ="home-title">hi</div>
             <div className = "home-content">
                 <p className = "home-content-1">I'm Luis,</p>
                 <p className = "home-content-2">a front-end developer</p>
